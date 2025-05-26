4a3f6fb75a0bfa4617f41d83e8ff7f6d2ce324e1
ARNESH DEY <arnesh93d@gmail.com>
2025-05-26 20:38:48 +0530

    Update main.tf

--- api/aws-app/local/main.tf
+++ api/aws-app/local/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
+  profile = "gh-actions"
   region  = "eu-west-1"
 }
 
@@ -51,10 +51,20 @@ resource "aws_instance" "sample_server" {
   key_name               = aws_key_pair.sample_key.key_name
 
   tags = {
-    "Name" = "sample_server"
+    "Name" = "dotnetwebapi"
   }
-}
+  site_config {
+    # site_config options: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_web_app#always_on
+    always_on = false # must be false for F1 (free)
 
+    # legacy app_service used linux_fx_version: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/app_service#linux_fx_version
+    #   i.e. linux_fx_version = "DOCKER|dockerbot93/actions-web-test:latest"
+    #   use application_stack instead:
+    application_stack {
+      docker_image_name   = "dockerbot93/actions-web-test:latest"
+      docker_registry_url = "https://index.docker.io" 
+    }
+}
 output "sample_server_dns" {
   value = aws_instance.sample_server.public_dns
 }
